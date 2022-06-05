9a79bbb959c6259c1446e60df2f29a7c657f6a42
rossouw-dynamism <106867910+rossouw-dynamism@users.noreply.github.com>
2022-06-05 11:05:07 +0200

    serving a response on root request

--- myApp.js
+++ myApp.js
@@ -5,4 +5,9 @@ let app = express();
 
 console.log("Hello World")
 
+app.get('/', (req, res) => {
+    res.send('Hello Express')
+    console.log("sending Hello Express in the response")
+  })
+
 module.exports = app;
